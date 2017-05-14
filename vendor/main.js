b1b7a2e504a6fdd1803c570414f71a71a9c307f7
Troy David Cook <www.troydavidcook@gmail.com>
2017-05-14 13:40:24 -0400

    Adding all pertinent files and assets... Its' been awhile : (

--- vendor/main.js
+++ vendor/main.js
@@ -9,13 +9,15 @@ $(document).ready(function (){
 
   var allLocations = {
     locationsToChooseFrom: [
-      "Gun Shop",
-      "Small Home",
+      "Alamo Guns",
+      "Work",
       "Suburban Home",
       "Under the Bridge",
       "Restaurant",
-      "Motel: Side of the Road",
-      "Police Station"
+      "Crappy Motel",
+      "Technoir: Club",
+      "Police Station",
+      "Apartment"
     ]
   };
 
@@ -114,44 +116,61 @@ $(document).ready(function (){
   var chosenLocations = [];
   // Function to choose 6 random locations to push to buttons. Not pushed yet.
   var randomLocation = function(array) {
-    for(let i = 0; i < 6; i++){
+    // Making sure ALAMO GUNS is always present, so user can get ammo at the beginning of the game, every playthrough.
+    chosenLocations[0] = allLocations.locationsToChooseFrom[0];
+    var gunStore = array.splice(array[0], 1);
+    for(let i = 1; i < 6; i++){
       var randomIndex = Math.ceil(Math.random() *  array.length - 1);
 
       chosenLocations.push(array[randomIndex]);
+//Time to append the .image_connor class to a random location
 
       array.splice(randomIndex, 1);
+      // if(chosenLocations[0] === allLocations.locationsToChooseFrom[0]) {
+      //   allLocations.locationsToChooseFrom[0].addClass(".image_connor");
+      // }
     }
+    array.splice(randomIndex);
+    console.log(randomIndex, "SPLICED INDEX");
   };
-
   randomLocation(allLocations.locationsToChooseFrom);
   console.log(chosenLocations, "CHOSEN LOCATIONS");
 
+
+// Give random index of chosenLocations the class of .image_connor and image-reese
+
+// Next we'll give an overlay with instructions!
+
+
+
+
+// Function to push the 6 random locations to the button divs.
 
   var pushToButtons = function() {
 
     var current = Math.ceil(Math.random() *  chosenLocations.length - 1);
 
-    $("#top-left-button").html(chosenLocations[current]).css("font-family", "terminator").css("color", "white").css("text-shadow", "2px 2px #000000").css("text-align", "center").css("padding-top", "25px").css("padding-left", "5px").css("padding-right", "10px");
+    $("#top-left-button").html(chosenLocations[current]).addClass("button-text");
     chosenLocations.splice(current, 1);
     console.log(chosenLocations, "CHOSEN LOCATIONS 1");
     current = Math.ceil(Math.random() *  chosenLocations.length - 1);
 
-    $("#top-middle-button").html(chosenLocations[current]).css("font-family", "terminator").css("color", "white").css("text-shadow", "2px 2px #000000").css("text-align", "center").css("padding-top", "25px").css("padding-left", "5px").css("padding-right", "10px");
+    $("#top-middle-button").html(chosenLocations[current]).addClass("button-text");
     chosenLocations.splice(current, 1);
     console.log(chosenLocations, "CHOSEN LOCATIONS 2");
     current = Math.ceil(Math.random() *  chosenLocations.length - 1);
 
-    $("#top-right-button").html(chosenLocations[current]).css("font-family", "terminator").css("color", "white").css("text-shadow", "2px 2px #000000").css("text-align", "center").css("padding-top", "25px").css("padding-left", "5px").css("padding-right", "10px");
+    $("#top-right-button").html(chosenLocations[current]).addClass("button-text");
     chosenLocations.splice(current, 1);
     console.log(chosenLocations, "CHOSEN LOCATIONS 3");
     current = Math.ceil(Math.random() *  chosenLocations.length - 1);
 
-    $("#bottom-left-button").html(chosenLocations[current]).css("font-family", "terminator").css("color", "white").css("text-shadow", "2px 2px #000000").css("text-align", "center").css("padding-top", "25px").css("padding-left", "5px").css("padding-right", "10px");
+    $("#bottom-left-button").html(chosenLocations[current]).addClass("button-text");
     chosenLocations.splice(current, 1);
     console.log(chosenLocations, "CHOSEN LOCATIONS 4");
     current = Math.ceil(Math.random() *  chosenLocations.length - 1);
 
-    $("#bottom-middle-button").html(chosenLocations[current]).css("font-family", "terminator").css("color", "white").css("text-shadow", "2px 2px #000000").css("text-align", "center").css("padding-top", "25px").css("padding-left", "5px").css("padding-right", "10px");
+    $("#bottom-middle-button").html(chosenLocations[current]).addClass("button-text");
     chosenLocations.splice(current, 1);
     console.log(chosenLocations, "CHOSEN LOCATIONS 5");
     current = Math.ceil(Math.random() *  chosenLocations.length - 1);
@@ -163,9 +182,17 @@ $(document).ready(function (){
   };
 
 
+var addConnor = function () {
+  let buttons = ["#bottom-right-button","#bottom-middle-button","#bottom-left-button","#top-right-button","#top-middle-button","#top-right-button"];
+  let randomIndex = Math.ceil(Math.random() *  array.length - 1);
+  console.log(buttons[0]);
+};
+addConnor();
 
 
   $(".yellow-pages").on("click", pushToButtons);
+
+
 });
 
 
